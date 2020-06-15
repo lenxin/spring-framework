f1cb2df4a2d0f4a39b4d7357f6f996811c5098da
yangxin <623685463@qq.com>
2020-06-15 11:06:56 +0800

    update

--- spring-context/src/main/java/org/springframework/context/support/SimpleThreadScope.java
+++ spring-context/src/main/java/org/springframework/context/support/SimpleThreadScope.java
@@ -1,31 +1,14 @@
-/*
- * Copyright 2002-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.context.support;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
 import org.springframework.core.NamedThreadLocal;
 import org.springframework.lang.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple thread-backed {@link Scope} implementation.
@@ -48,11 +31,10 @@ import org.springframework.lang.Nullable;
  *
  * @author Arjen Poutsma
  * @author Juergen Hoeller
- * @since 3.0
  * @see org.springframework.web.context.request.RequestScope
+ * @since 3.0
  */
 public class SimpleThreadScope implements Scope {
-
 	private static final Log logger = LogFactory.getLog(SimpleThreadScope.class);
 
 	private final ThreadLocal<Map<String, Object>> threadScope =
@@ -62,7 +44,6 @@ public class SimpleThreadScope implements Scope {
 					return new HashMap<>();
 				}
 			};
-
 
 	@Override
 	public Object get(String name, ObjectFactory<?> objectFactory) {
@@ -98,5 +79,4 @@ public class SimpleThreadScope implements Scope {
 	public String getConversationId() {
 		return Thread.currentThread().getName();
 	}
-
 }
